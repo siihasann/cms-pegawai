d0a8352bcd1337e6937aa1e8fc44e043dd5128b4
siihasann <hasannajib3@gmail.com>
2024-11-10 21:22:58 +0700

    fix deploy 11

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Cek jika lingkungan aplikasi adalah produksi
+        if (env('APP_ENV') == 'production') {
+            // Memaksa penggunaan HTTPS pada semua URL
+            URL::forceScheme('https');
+        }
     }
 }
