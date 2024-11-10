e94353af0a288164b940e127419906df0510e274
siihasann <hasannajib3@gmail.com>
2024-11-10 21:18:05 +0700

    fix deploy 10

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+
             refresh: true,
         }),
         vue({
@@ -17,6 +21,9 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        outDir: 'public/build',
+    },
     resolve: {
         alias: {
           ziggy: '/vendor/tightenco/ziggy/dist/vue.m.js',
